20ae0c3d84326eccf80801eadce63630f971dfc3
torwalds <zelenov_denis@usp.ua>
2017-12-07 17:14:43 +0200

    stable 0.4
    added second  ADC, testing plot

--- log.py
+++ log.py
@@ -1,8 +1,13 @@
 import csv
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
 
 i = 0
-
-dbvalues = []
+xar = []
+yar = []
+X = []
+Y = []
 database = "c:\\files\\project_master\\log.csv"
 
 with open(database) as db:
@@ -10,8 +15,24 @@ with open(database) as db:
     header = next(ejectdata)
     for row in ejectdata:
         try:
-            dbvalues.append(row[1])
+            if i % 2 == 0:
+                yar.append(row[0])
+                xar.append(row[1])
+            else:
+                Y.append(row[0])
+                X.append(row[1])
             i = i + 1
         except IndexError:
             break
-print(dbvalues)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+    ax1.clear()
+    #ax2.clear()
+    ax1.plot(xar,yar)
+    ax1.plot(X, Y, c ='red')
+ani = animation.FuncAnimation(fig, animate, interval=30)
+
+plt.show()
--- main.py
+++ main.py
@@ -17,11 +17,15 @@ def update_temp(queue):
     global i
     while True:
         i = i + 0.005
-        tempread=ser.readline(4)
-        strdata = str(tempread, 'utf-8')
-        logfile.writelines(strdata + "," + str("%.3f" % i) +"," + "\n")
-        queue.put(tempread)
-        #time.sleep(0.01)
+        tempread_1 = ser.readline(4)
+        tempread_2 = ser.readline(4)
+        strdata_1 = str(tempread_1, 'utf-8')
+        strdata_2 = str(tempread_2, 'utf-8')
+        logfile.writelines(strdata_1 + "," + str("%.3f" % i) +"," + "\n")
+        logfile.writelines(strdata_2 + "," + str("%.3f" % i) +"," + "\n")
+        queue.put(tempread_1)
+        queue.put(tempread_2)
+        #time.sleep(0.001)
 
 class Gui(object):
     def __init__(self, queue):
@@ -34,21 +38,31 @@ class Gui(object):
         self.root.geometry('%dx%d+%d+%d' % (400, 400, 500, 500))
 
         self.equipTemp = StringVar()        
-
+        self.equipTemp1 = StringVar() 
         
-        labelpass = Label(self.root, font=("Impact", 20), foreground = "black", text="Current value: ")
+        labelpass = Label(self.root, font=("Impact", 20), foreground = "black", text="Value first: ")
         labelpass.place(x=0,y=0)
+
         Label2=Label(self.root, textvariable=self.equipTemp, font=("Impact", 20), foreground = "red", justify=RIGHT)
         Label2.place(x=180, y=0)
+
+        labelpass2 = Label(self.root, font=("Impact", 20), foreground = "black", text="Value second: ")
+        labelpass2.place(x=0,y=50)
+
+        Label3=Label(self.root, textvariable=self.equipTemp1, font=("Impact", 20), foreground = "red", justify=RIGHT)
+        Label3.place(x=180, y=50)
+
         bQuit = Button(self.root, text="Quit", command=self.root.quit)
-        bQuit.place(x = 0, y = 50)
+        bQuit.place(x = 0, y = 100)
 
         self.root.after(300, self.read_queue)
 
     def read_queue(self):
         try:
-            temp = self.queue.get_nowait()
-            self.equipTemp.set(temp)
+            temp1 = self.queue.get_nowait()
+            temp2 = self.queue.get_nowait()
+            self.equipTemp.set(temp1)
+            self.equipTemp1.set(temp2)
         except qe.Empty:
             pass
 
